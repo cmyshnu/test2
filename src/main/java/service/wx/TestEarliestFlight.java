8eb552a550cf9e22f07ee57de8bf62ad100710df
同程网络-樊宇39919 <fy39919@tcent.cn>
2018-01-23 17:52:31 +0800

    fy39919

--- src/main/java/service/wx/TestEarliestFlight.java
+++ src/main/java/service/wx/TestEarliestFlight.java
@@ -1,6 +1,7 @@
 package assertfile;
 
 import common.AnalyzeResponse;
+import common.NowTime;
 import common.flightinfo.cabinsInfoVO;
 import common.flightinfo.flightInfoVO;
 import org.testng.annotations.DataProvider;
@@ -8,12 +9,18 @@ import org.testng.annotations.Test;
 import service.wx.GetWxResponse;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class Assert {
 
     @Test
-    public void testWxFlightOffTime(){
+    public void testWxFlightOffTime() throws ParseException {
+        NowTime nowTime = new NowTime();
+        String time1 = nowTime.nowTime();
+        System.out.println(time1);
         AnalyzeResponse analyzeResponse = new AnalyzeResponse();
         List<String> list = new ArrayList<String>();
         Map<flightInfoVO, cabinsInfoVO> map = analyzeResponse.analyzeResponse();
@@ -21,11 +28,15 @@ public class Assert {
             list.add(entry.getKey().getFlightOffTime());
         }
         Collections.sort(list);
-        String time = list.get(0);
-        System.out.println(time);
 
+
+        String time = list.get(0).toString();
+        System.out.println(time);
+        DateFormat df = new SimpleDateFormat("HH:mm");
+        Date dt1 = df.parse(time);
+        Date dt2 = df.parse(time1);
+
+        assert dt1.getTime()<dt2.getTime();
 
     }
-
-
 }
